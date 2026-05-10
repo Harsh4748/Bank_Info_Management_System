6d78d05096247f3cd145feceb8aabb72f2460f11
HarshAgrwal <harshagrawal4748@gmail.com>
2026-05-10 09:46:38 +0530

    solve bugs

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
 mongoose
